08bcf813fc2ec670443db659c6ff24b4fcd3a517
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-23 18:25:53 +0700

    void output

--- intoOOP/intoOOP.cpp
+++ intoOOP/intoOOP.cpp
@@ -6,4 +6,11 @@ public: // akses modifier
 	string nama;
 	int umur;
 	string jurusan;
+
+	void output() {
+		cout << "nama: " << nama << endl;
+		cout << "umur: " << umur << endl;
+		cout << "jurusan: " << jurusan << endl;
+
+	}
 };
